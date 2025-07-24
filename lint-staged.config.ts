2f709cda9d2b054a7116d81363214479fb09dfa6
Dawid Drelichowki <dawid.drelichowski@gmail.com>
2025-07-24 19:17:55 +0200

    Addition of Husky and Lint staged

---
+++ lint-staged.config.ts
@@ -0,0 +1,13 @@
+import type { Configuration } from 'lint-staged'
+
+const config: Configuration = {
+  '*.{js,ts,tsx,css,json,md}': [
+    'eslint --fix',
+    'prettier --write',
+  ],
+  '*.{yml}': [
+    'prettier --write',
+  ],
+}
+
+export default config
